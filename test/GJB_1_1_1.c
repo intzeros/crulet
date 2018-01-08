e8f3b1203b959e90f94a5acae700a8aace9b299b
lszero <intzero@outlook.com>
2018-01-08 15:06:31 +0800

    update test

---
+++ test/GJB_1_1_1.c
@@ -0,0 +1,7 @@
+void foo(unsigned int p_1){
+    unsigned int x = p_1;
+}
+
+void static_p(void){
+    unsigned int foo = 1u;
+}
--- test/test.c
+++
@@ -1,6 +0,0 @@
-void static_p(void){
-    long arr[10] = {100};
-    const unsigned int y = 64u;
-    arr[0] <<= y;
-    arr[1] = y >> 32;
-}
