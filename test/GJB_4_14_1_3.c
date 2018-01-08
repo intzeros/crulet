b3cd41a3ead5cde529acf322df0f2d44bd4ea348
lszero <intzero@outlook.com>
2018-01-08 14:18:11 +0800

    add GJB-rule-4.14.1.3

---
+++ test/GJB_4_14_1_3.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+
+void static_p(void){
+    bool flag = false;
+    /*...*/
+    switch(flag){
+        case true:
+            /*...*/
+            break;
+        case false:
+            /*...*/
+            break;
+        default:
+            break;
+    }
+}
