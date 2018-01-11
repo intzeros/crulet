03e916bdf6386551ab5dd2d51ffccadce40b3407
lszero <intzero@outlook.com>
2018-01-11 16:52:13 +0800

    update test

---
+++ test/GJB_1_1_5.c
@@ -0,0 +1,6 @@
+unsigned int static_p(p_1){
+    unsigned int result;
+    /*...*/
+    result = p_1 * 2;
+    return result;
+}
---
+++ test/GJB_1_1_6.c
@@ -0,0 +1,10 @@
+int static_p();
+
+int static_p(unsigned int p){
+    int x = 1;
+    /*...*/
+    if(p == 0){
+        x = 0;
+    }
+    return x;
+}
